d14c7fed111b1f6feb58f9368dfc015b298009d0
Abhishek AN <darkabhi1520@gmail.com>
2021-06-01 23:29:21 +0530

    remove unused routes
    Signed-off-by: Abhishek AN <darkabhi1520@gmail.com>

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const { MONGO_URI } = require("./config");
 //require('dotenv').config();
+
 // Routes
 const ambulanceRoutes = require("./routes/api/ambulance");
 const bloodDonorRoutes = require("./routes/api/blood");
@@ -10,14 +11,14 @@ const foodRoutes = require("./routes/api/food");
 const homeTestingRoutes = require("./routes/api/hometesting");
 const onlineDocRoutes = require("./routes/api/onlinedoc");
 const oxygenRoutes = require("./routes/api/oxygen");
-const plasmaRoutes = require("./routes/api/plasma");
 const teleCounsellingRoutes = require("./routes/api/tele");
+
+// Authentication
 const registrationRoutes = require("./routes/api/register");
 const loginRoutes = require("./routes/api/login");
 
+// Feedback
 const feedbackRoutes = require("./routes/api/feedback");
-
-const foodcatererRoutes = require("./routes/api/foodcaterer")
 
 // Medicine tab routes
 const jakBangaloreRoutes = require("./routes/api/jakbangalore")
@@ -50,19 +51,22 @@ app.use("/api/food", foodRoutes);
 app.use("/api/hometesting", homeTestingRoutes);
 app.use("/api/onlinedoc", onlineDocRoutes);
 app.use("/api/oxygen", oxygenRoutes);
-app.use("/api/plasma", plasmaRoutes);
 app.use("/api/tele", teleCounsellingRoutes);
 
-app.use("/api/register", registrationRoutes);
-app.use("/api/login", loginRoutes);
-
-app.use("/api/feedback", feedbackRoutes);
-app.use("/api/foodcaterer", foodcatererRoutes)
-
+// Medicine routes
 app.use("/api/jakbangalore", jakBangaloreRoutes)
 app.use("/api/jakkarnataka", jakKarnatakaRoutes)
 app.use("/api/medicineoffline", medicineOfflineRoutes)
 app.use("/api/medicineonline", medicineOnlineRoutes)
+
+// Auth routes
+app.use("/api/register", registrationRoutes);
+app.use("/api/login", loginRoutes);
+
+// Feedback route
+app.use("/api/feedback", feedbackRoutes);
+
+
 
 // Listen to server
 app.listen(process.env.PORT || 11000, () => console.log("Server Started"));
